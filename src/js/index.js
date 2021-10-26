1217b0ae6c0a1a002397936aba08b51fd8b9f638
Ze Zhou <zezhou96@gmail.com>
2021-10-25 19:16:56 -0600

    fixed refresh on toDoPage

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,12 @@ import Router from "./router/router";
 import dataFetcher from "./utils/dataFetcher";
 
 const app = document.querySelector("#app")
-Router(window.location.pathname)
 
 const onAppInit = async function(e){
     let todoData = await dataFetcher()
 
     createStore(todoData)
+    Router(window.location.pathname)
 }
 
 window.addEventListener('load', onAppInit)
